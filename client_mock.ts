118af66c075b27db3f150a7fc0174fb189a176f2
Yuxuan Xie <teststaybaka@gmail.com>
2024-09-27 10:26:49 -0700

    Add mock client. Release

---
+++ client_mock.ts
@@ -0,0 +1,19 @@
+import { NodeServiceClient } from "./client";
+
+export class NodeServiceClientMock extends NodeServiceClient {
+  public request: any; // captured
+  public error: Error; // to throw
+  public response: any; // to return;
+
+  public constructor() {
+    super(undefined);
+  }
+  public async send(request: any): Promise<any> {
+    this.request = request;
+    if (this.error) {
+      throw this.error;
+    } else {
+      return this.response;
+    }
+  }
+}
